cfc840a1f0caf35d8ef4cbf0af6cbd46daad521c
Lea Cassegrain <lea.csgn@gmail.com>
2023-07-03 00:33:39 +0200

    new component url corrected

--- front/src/pages/new/New.jsx
+++ front/src/pages/new/New.jsx
@@ -1,9 +1,10 @@
-import "../new/new.css";
+import React, { useState } from "react";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { FaFolderPlus } from 'react-icons/fa';
 import Navbar from "../../components/navbar/Navbar";
-import { useState } from "react";
-import axios from "axios";
+
+import "../new/new.css"; // Assuming this is the correct path to your CSS file
 
 const New = ({ inputs, title }) => {
   const [file, setFile] = useState("");
@@ -29,16 +30,16 @@ const New = ({ inputs, title }) => {
         "https://back.cloudinary.com/v1_1/ddfkz42vp/image/upload",
         data
       );
-  
+
       const { url } = uploadRes.data;
-  
+
       const newUser = {
         ...info,
         img: url,
       };
-  
-      await axios.post("/auth/register", newUser);
-      setShowMessage(true)
+
+      await axios.post("https://booking-app-ag0s.onrender.com/back/auth/register", newUser);
+      setShowMessage(true);
       setTimeout(() => {
         navigate("/"); // Redirect to the home page after a delay
       }, 2000); // Delay in milliseconds (2 seconds)
@@ -46,33 +47,32 @@ const New = ({ inputs, title }) => {
       console.log(err.message);
     }
   };
-  
 
   console.log(info);
-  
+
   return (
     <div className="new">
-      <Navbar/>
+      <Navbar />
       <div className="newContainer">
         <div className="bannerNew">
           <h2>{title}</h2>
         </div>
         <div className="bottomNew">
           <div className="uploadNew">
-          {showMessage && (
-        <div className="message">Account created! You can now log in.</div>
-        )} 
+            {showMessage && (
+              <div className="message">Account created! You can now log in.</div>
+            )}
             <img
               src={
                 file
                   ? URL.createObjectURL(file)
-                  : require("../../assets/user.png")
+                  : require("../../assets/user.png").default
               }
               alt=""
             />
-               <label htmlFor="file">
-                <FaFolderPlus className="icon" style={{ width: "24px", height:"24px", color:"#003B95" }} />
-                </label>
+            <label htmlFor="file">
+              <FaFolderPlus className="icon" style={{ width: "24px", height: "24px", color: "#003B95" }} />
+            </label>
           </div>
 
           <div className="formNew">
